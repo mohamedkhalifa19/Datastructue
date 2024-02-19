7a7343bc7fd279e211694a62d6b5c48d6b1baed8
Mohamed Khalifa <mohamedabdelkader01022856467@gmail.com>
2024-02-19 22:54:03 +0200

    first Day

---
+++ codes/first_session/index.js
@@ -0,0 +1,14 @@
+const arr = [1, "good", true];
+
+// push O(1)
+arr.push(445);
+console.log(arr);
+// pop o(1)
+arr.pop();
+console.log(arr);
+// add from begin O(n)
+arr.unshift(45);
+console.log(arr);
+// remove from end O(n)
+arr.shift();
+console.log(arr);
